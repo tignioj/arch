e857ec12e523f81123dc6d939f4a413e85d9d1ad
Arch-xfce-Virtual <noemail@example.com>
2018-05-07 18:57:49 +0800

    support BIOS& UEFI,add detected your machinew

--- beforechroot.sh
+++ beforechroot.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
-#time=========================================
+#setting---time=========================================
+
+if [[ ls /sys/firmware/efi/efivars ]]
+then
+	echo "Your device support UEFI"
+else
+	echo -e "Your device UEFI is unable to start, If you want to Install Archlinux by UEFI, Please shutdown this Virtual-machine and checkout you Virtual setting,following by these step"
+	echo -e "Vm--->Settings--->Options---->Advanced---->UEFI"
+	echo "But.. you can also Install Archlinux by BIOS"
+	echo -e  "1.yes\n2.no"
+	read -p "continue by BIOS?(Please choose a number in 5s,default is 1)" -t 5 IF_CONTINUE
+	if [[ $? -eq 142 ]]
+	then
+	MYTYPE=2
+	elif [[ $IF_CONTINUE -eq 1 ]]
+	then
+		echo "you choose $IF_CONTINUE"
+	elif [[ $IF_CONTINUE -eq 2 ]]
+	then
+		echo "you choose $IF_CONTINUE"
+		exit
+	fi
+fi
+
+
 echo "step1.==========setting time..."
 timedatectl set-ntp true
 timedatectl set-timezone Asia/Shanghai
 timedatectl set-ntp true
 hwclock --systohc --utc
 
+echo "ready to FORMATTING===========================>>"
+read -p "do you wang to skip(1) this setp(in 5s)?" -t 5 WHETHER_JUMP_FORMAT
+#========================skip(1)-TO-row:109=======================
+MY_INPUT_STATE=$?
+if [[ $MY_INPUT_STATE -eq 142 ]]
+then
+	echo "Don't skip"
 #block=======================================no home block=
 echo "step2.==========block-formatting"
 lsblk
-echo -e "choose your block (default is /dev/sda) \n\n NOTICE:it will clear all of your content from Udisk!\n\n"
-read -p "Enter you block(in 10s ):" -t 10 MY_BLOCK
+echo -e "choose your block (default is /dev/sda) \n\n NOTICE:This operation will erase all the date in your U disk!!!\n\n"
+read -p "Enter you block(in 15s ):" -t 15 MY_BLOCK
 BLOCK_STATE=$?
 if [[ $BLOCK_STATE -eq 142 ]]
 then
@@ -18,18 +49,26 @@ then
 fi
 
 echo "your block is $MY_BLOCK"
-echo -e "1.UEFI\n2.BIOS"
-read -p "Input your number(in 5s)(default is 1):" -t 5 MYTYPE
-MY_INPUT_STATE=$?
-if [[ $MY_INPUT_STATE -eq 142 ]]
-then
-	MYTYPE=1
-fi
 
+if [[ $MYTYPE -eq 1 ]]
+then
+	#choose your type==============================
+	echo "your machine support BIOS AND UEFI"
+	echo -e "1.UEFI\n2.BIOS"
+	read -p "Input your number(in 10s)(default is 1):" -t 10 MYTYPE
+	MY_INPUT_STATE=$?
+
+	if [[ $MY_INPUT_STATE -eq 142 ]]
+	then
+		MYTYPE=1
+	fi
+fi
 #=================UEFI============================
 if [[ $MYTYPE -eq 1 ]]
 then
 echo "====gdisk===="
+umount /mnt/boot
+umount /mnt
 sudo gdisk $MY_BLOCK << EOF
 o
 y
@@ -66,6 +105,8 @@ fi
 #================BIOS============================
 if [[ $MYTYPE -eq 2 ]]
 then
+umount /mnt/boot
+umount /mnt
 fdisk $MY_BLOCK << EOFBIOS
 d
 1
@@ -96,6 +137,7 @@ echo "done"
 fi
 lsblk
 fdisk -l
+fi #//skip(1)-to-here===============
 echo "change rope================(default:tuna)"
 if test -e /etc/pacman.d/mirrorlist_bak
 then
